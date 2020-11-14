8e11d4efd6fc9e1f581d3d4e67b10d8b8ff21079
TheCrown <a1156883061@outlook.com>
2020-11-14 21:33:24 +0800

    修复单独引入样式问价出错

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   // 选项...\
   configureWebpack: {
-    target: "electron-renderer"
-  }
+    target: 'electron-renderer',
+  },
+  css: {
+    loaderOptions: {
+      // 给 less-loader 传递 Less.js 相关选项
+      less: {
+        // http://lesscss.org/usage/#less-options-strict-units `Global Variables`
+        // `primary` is global variables fields name
+        javascriptEnabled: true,
+      },
+    },
+  },
 };
