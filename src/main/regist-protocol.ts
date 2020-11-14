2b451faf507e8adeb6ae3a5f0f87c881ab961d69
TheCrown <a1156883061@outlook.com>
2020-11-14 21:35:14 +0800

    更改换行符为lf

--- src/main/regist-protocol.ts
+++ src/main/regist-protocol.ts
@@ -1,13 +1,13 @@
-import { protocol } from "electron";
-import path from "path";
+import { protocol } from 'electron';
+import path from 'path';
 
 export function registerSafeFileProtocol() {
-  protocol.registerFileProtocol("safe-file", async (filePath, callback) => {
-    const url = filePath.url.replace("safe-file://", "");
+  protocol.registerFileProtocol('safe-file', async (filePath, callback) => {
+    const url = filePath.url.replace('safe-file://', '');
     try {
       return callback({ path: path.normalize(decodeURI(url)) });
     } catch (error) {
-      console.log("load file error", error);
+      console.log('load file error', error);
     }
   });
 }
