2949edf00ad259dcb42c937ab6d17f6ee5c027fe
TheCrown <a1156883061@outlook.com>
2020-11-19 01:13:25 +0800

    添加多语言，删除不必要标签

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-    <router-view v-slot="{ Component }">
-      <keep-alive include="HelloWorld">
-        <component :is="Component" />
-      </keep-alive>
-    </router-view>
-  </div>
+  <a-config-provider :locale="locale">
+    <div id="nav">
+      <router-view v-slot="{ Component }">
+        <keep-alive include="HelloWorld">
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+    </div>
+  </a-config-provider>
 </template>
+
+<script lang="ts">
+  import zhCN from 'ant-design-vue/es/locale/zh_CN';
+  import moment from 'moment';
+  import 'moment/dist/locale/zh-cn';
+  import { defineComponent, ref, watch } from 'vue';
+
+  moment.locale('zhCN');
+
+  export default defineComponent({
+    setup() {
+      const locale = ref(zhCN);
+      watch(locale, (newValue) => {
+        moment.locale(newValue.locale);
+      });
+      return {
+        locale,
+      };
+    },
+  });
+</script>
 
 <style lang="less">
   #app {
@@ -18,18 +38,11 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    height: 100%;
   }
 
   #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
+    padding-top: 16px;
+    height: 100%;
   }
 </style>
