9ba4562c96084f92248f4495fb74bc80ece8fe83
TheCrown <a1156883061@outlook.com>
2021-04-18 20:26:24 +0800

    feat: 新增nsis打包配置

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,15 @@ const webConfig = {
       builderOptions: {
         target: 'dir',
         win: {
-          target: 'dir',
+          target: 'nsis',
+        },
+        nsis: {
+          oneClick: false, // 一键安装
+          perMachine: true, // 是否开启安装时权限限制（此电脑或当前用户）
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true, // 创建开始菜单图标
         },
         asarUnpack: ['0.worker.js'],
       },
